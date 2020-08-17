636b9b4cc2f907688728f988b382d8e23c2cd209
nitaisutradhar <68436890+nitaisutradhar@users.noreply.github.com>
2020-08-17 08:55:29 +0600

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,20 @@
+var resultField=$("#result");
+function insertNumber(number){
+	var existingNumbers=resultField.val();
+	resultField.val(existingNumbers+number);
+}
+function clearResult(){
+	resultField.val('');
+}
+function calculate(){
+	var result=eval(resultField.val());
+	resultField.val(result);
+}
+function deleteNumber(){
+	var presentValue=resultField.val();
+	resultField.val(presentValue.slice(0,-1))
+}
+function changeNumber(){
+	var value=(resultField.val()*(-1));
+	resultField.val(value);
+}
